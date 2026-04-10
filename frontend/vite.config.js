bc934b68e52b2e999f3ec772db87cbef4aa9b162
amansingh-web <amansingh-web>
2026-04-11 01:57:16 +0530

    fixed API URL to production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://shopkart-apka-ecommerce.onrender.com',
         changeOrigin: true,
       },
     },
