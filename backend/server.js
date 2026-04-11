bfe37f5d60e75e3f5cc96adcf0320ce7a9f60932
amansingh-web <amansingh-web>
2026-04-11 21:47:43 +0530

    fix render port final

--- backend/server.js
+++ backend/server.js
@@ -53,10 +53,12 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB Connected');
+
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () =>
-      console.log(`🚀 Server running on http://localhost:${PORT}`)
-    );
+
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error('❌ MongoDB connection error:', err.message);
